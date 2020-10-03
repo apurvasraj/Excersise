3b4bb775e82eea0f351c205db4bb3a9db6663cd8
apurvasraj <71725293+apurvasraj@users.noreply.github.com>
2020-10-03 16:42:00 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,29 @@ app.get('/', function(req, res, next) {
 //PUT INSIDE YOUR index.js file (or whereever)
 // Assumes DATABASE_URL in the heroku configs points to your PostGres database
 // setups the url /db to connect to database and does a query from a table test_table
-var pg = require('pg');
-var pool = new pg.Pool()
 
-app.get('/db', function (request, response) {
-  pool.connect(function(err, client, done) {
-  //pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM \"test_table\"', function(err, result) {
-      done();
-      if (err)
-      { console.error(err); response.send("Error " + err); }
-      else
-      { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 module.exports = app;
